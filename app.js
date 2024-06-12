3842921d2503dbfafe96781a552ce926ce923c47
AbdulSamadNadeem <samadnadeem372@gmail.com>
2024-06-12 15:50:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+let like = document.getElementById('like')
+let laugh = document.getElementById('laugh')
+let love = document.getElementById('love')
+let counts = document.getElementById('counts')
+let number = 0
+function colorchangelike(){
+
+   like.style.color="#3498db"
+   laugh.style.color="#000"
+   love.style.color="#000"
+
+    
+    
+        counts.innerHTML=`Reacts:${number=number + 1}`
+
+}
+function colorchangelaugh(){
+
+   laugh.style.color="#f1c40f"
+   like.style.color="#000"
+   love.style.color="#000"
+    
+
+   
+        counts.innerHTML=`Reacts:${number=number + 1}`
+
+   
+}
+function colorchangelove(){
+
+   love.style.color="#c0392b"
+   laugh.style.color="#000"
+    like.style.color="#000"
+    
+
+  counts.innerHTML=`Reacts:${number=number + 1}`
+
+
+}
